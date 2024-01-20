8b34c2e761fc25ec0af0258c6184d3c99ae8637e
josuan_fioroto <142553191+josuanfioroto@users.noreply.github.com>
2024-01-20 00:24:22 -0300

    pokemons_dio

---
+++ pokémons.js
@@ -0,0 +1,66 @@
+let pokemon1 = "Poochyena"
+let level = 2 
+let hp = 13
+let sexo = "M"
+let tipo = "Noturno"
+let fraqueza = "Lutador,Inseto,Fada"
+let numeropokedex = 261
+let podeBatalhar = true
+
+console.log(pokemon1 + "Cadastrado com sucesso")
+
+let pokemon2 = "Zigzagoon"
+let level = 2
+let hp = 13
+let sexo = "F"
+let tipo = "Normal"
+let fraqueza = "Lutador"
+let numeropokedex = 263
+let podeBatalhar = true
+
+console.log(pokemon2 + "Cadastrado com sucesso")
+
+let pokemon3 = "Dragonite"
+let level = 5
+let hp = 25
+let sexo = "M"
+let tipo = "Dragão,Voador"
+let fraqueza = "Gelo,Pedra,Dragão,Fada"
+let numeropokedex = 149
+let podeBatalhar = true
+
+console.log(pokemon3+ "Cadastrado com sucesso")
+
+let pokemon4 = "Dragonite"
+let level = 5
+let hp = 24
+let sexo = "F"
+let tipo = "Dragão,Voador"
+let fraqueza = "Gelo,Pedra,Dragão,Fada"
+let numeropokedex = 149
+let podeBatalhar = true
+
+console.log(pokemon4 + "Cadastrado com sucesso")
+
+let pokemon5 = "Poochyena"
+let level = 3
+let hp = 15
+let sexo = "F"
+let tipo = "Noturno"
+let fraqueza = "Lutador,Inseto,Fada"
+let numeropokedex = 261
+let podeBatalhar = true
+
+console.log(pokemon5 + "Cadastrado com sucesso")
+
+let pokemon6 = "Wurmple"
+let level = 2
+let hp = 14
+let sexo = "M"
+let tipo = "Inseto"
+let fraqueza = "Fogo,Voador,Pedra"
+let numeropokedex = 265
+let podeBatalhar = true
+
+console.log(pokemon6 + "Cadastrado com sucesso")
+
